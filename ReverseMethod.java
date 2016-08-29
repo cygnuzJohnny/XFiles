595656556d7d63340bd5865d1e067eef6ace5a61
cygnuzJohnny <cygnuzJohnny@users.noreply.github.com>
2016-08-29 12:28:27 -0400

    Add files via upload

---
+++ ReverseMethod.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+/**Johnny Robinson
+ * cygnuz007@hotmail.com
+ */
+
+
+/**A method for reverse a number using recursion
+ * If the user enters 12345, the number will be converted to 54321
+ *
+ */
+public class ReverseMethod {
+
+	
+	
+	
+	public static void reverseMethod(int number) {
+		if (number < 10) {
+			   System.out.println(number);
+			   return;
+
+			}
+		else {
+			System.out.print(number % 10);
+        
+        reverseMethod(number / 10);
+        
+		}
+	   }
+	   public static void main(String args[])
+	   {
+		int num=0;
+		System.out.println("Input your number and press enter: ");
+		
+		//Create Scanner Object
+		Scanner input = new Scanner(System.in);
+		
+		//Wait for the user to enter something
+		num = input.nextInt();
+		System.out.print("Reverse of the input number is:");
+		reverseMethod(num);
+		System.out.println();
+		input.close();
+	   }
+
+}
