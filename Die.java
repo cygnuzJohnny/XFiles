af1aba8b6e644994efe6cb42989ae85ea8835ffe
Johnny Robinson <cygnuz007@gmail.com>
2016-09-13 14:46:45 -0400

    Die.java

---
+++ Die.java
@@ -0,0 +1,27 @@
+import java.lang.Math;
+
+
+
+public class Die {
+	private int sides;
+	private int roll;
+
+	public Die() {
+		sides = 0;
+		roll = 0;
+	}
+
+	public void setSides(int sides) {
+		this.sides = sides;
+	}
+
+	public int getSides() {
+		return sides;
+	}
+
+	public int rollDice(int s) {
+		roll = (int) Math.ceil(Math.random() * sides);
+		return roll;
+
+	}
+}
