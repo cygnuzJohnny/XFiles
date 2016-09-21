2667f2cc4ddde4c25347255a0d16432e5bee5db1
Johnny Robinson <cygnuz007@gmail.com>
2016-09-21 09:50:51 -0400

    CircleAPPS.java

--- CircleAPPS.java
+++ CircleAPPS.java
@@ -12,36 +12,43 @@ public class CircleAPPS {
 
 	public static void main(String[] args) {
 		
+		//Declare and initialize variables
 		double radius = 0;
 		int count = 0;
 		String choice = "y";
 		
+		//Create Scanner
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Welcome to the Circle Tester!");
 		
+		//Enter While Loop
 		while(choice.equalsIgnoreCase("y")) {
 			System.out.println("Enter radius: ");
-			radius = sc.nextDouble();
+			radius = sc.nextDouble();//Enter user input for radius
 			
+			
+			//Create object name circles
 			Circles circles = new Circles(radius);
 
+			
 			
 			System.out.println("Circumference: " + circles.getFormattedCircumference());
 			System.out.println("Area: " + circles.getFormattedArea());
 			
+			//Number of circles user built
 			count = circles.getObjectCount();
 			
 			System.out.println("would you like to create another Circle? (y/n): ");
-			choice = sc.next();
+			choice = sc.next();//Ask user input
 		}
 			System.out.println("You created " + count + " Circles");
 			System.out.println("Goodbye!");
 
-			sc.close();
+			sc.close();//Close scanner
 
 		// TODO Auto-generated method stub
 
-	}
+	}//Close main method
 
-}
+}//Close class
